227b1dd5588c98d467fa6f79734793d434d73619
Raju Wahyudi <rajuwahyudi842@gmail.com>
2025-03-08 16:16:07 +0700

    add constant color and base url

---
+++ lib/constants/style/color.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyColor {
+  static const kColorPrimary = Color(0xFFF3C76F);
+  static const kColorSecondary = Color(0xFFFFB88C);
+  static const kColorBg = Color(0xFF1D1D1D);
+  static const kColorBrand = Color(0xFFF3C76F);
+  static const kColorBgAccentDarker = Color(0xFF252526);
+  static const kColorBgAccent = Color(0xFF2C2C2C);
+  static const kColorBgAccent2 = Color(0xFF666666);
+  static const kColorText = Color(0xFFEEECEB);
+  static const kColorTextSecondary = Color(0xFF8B8B8B);
+  static const kColorTextButton = Color(0xFF000000);
+  static const kColorSecondaryText = Color(0xFFC4C4C4);
+}
---
+++ lib/constants/url.dart
@@ -0,0 +1,1 @@
+const baseUrl = 'https://customer.holywings.id/api/v1';
