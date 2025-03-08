2607cf64f9a6244c4e9cf15287e3a2e29c6c5ad9
Raju Wahyudi <rajuwahyudi842@gmail.com>
2025-03-09 03:52:07 +0700

    add screen size

--- lib/features/home/view/home_view.dart
+++ lib/features/home/view/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:homepage/constants/style/color.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +11,14 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: MyColor.kColorBg,
+        title: Column(
+          children: [Text('Hi, HWG People'), Text('Click to Login')],
+        ),
+      ),
+      body: Container(),
+    );
   }
 }
